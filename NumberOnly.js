84032746855484bcb2474575cd0f775308b23f1b
Jared <jaredjkim@gmail.com>
2017-11-14 21:11:56 -0800

    first commit

---
+++ NumberOnly.js
@@ -0,0 +1,14 @@
+
+function numbersOnly(arr)
+{
+    var newArray = [];
+    for(var i =0; i < arr.length; i++){
+        if( typeof arr[i] == "number")
+        {
+            newArray.push(arr[i]);
+        }
+        }
+        console.log(newArray);
+        return newArray;
+}
+var oldArray = numbersOnly([1, "apple", -3, "orange", 0.5]);
